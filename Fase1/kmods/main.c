e1d72aa7631951f0df504f09e01f1ddb26540b1a
ponche <josueguevara792@gmail.com>
2025-06-09 20:57:36 -0600

    Agregar archivos principales del kernel: main.c y uts.h con inicialización y definiciones del sistema

---
+++ Fase1/kmods/main.c
@@ -0,0 +1,36 @@
+    /*Linea 954 */
+	/*
+	 * Set up housekeeping before setting up workqueues to allow the unbound
+	 * workqueue to take non-housekeeping into account.
+	 */
+	housekeeping_init();
+
+	/*
+	 * Allow workqueue creation and work item queueing/cancelling
+	 * early.  Work item execution depends on kthreads and starts after
+	 * workqueue_init().
+	 */
+	workqueue_init_early();
+    printk(KERN_INFO "Bienvenido a usac-kernel-1128-7639-3856\n");
+	rcu_init();
+
+	/* Trace events are available after this */
+	trace_init();
+
+	if (initcall_debug)
+		initcall_debug_enable();
+
+	context_tracking_init();
+	/* init some links before init_ISA_irqs() */
+	early_irq_init();
+	init_IRQ();
+	tick_init();
+	rcu_init_nohz();
+	init_timers();
+	srcu_init();
+	hrtimers_init();
+	softirq_init();
+	timekeeping_init();
+	time_init();
+
+    /* Linea 987 */
